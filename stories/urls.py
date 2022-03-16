3ee13b5c5b6d6e10f81a8b53dd1fc7f30c279c43
jwjlord <99141066+jwjlord@users.noreply.github.com>
2022-03-16 23:47:55 +0000

    Initial framework

---
+++ stories/urls.py
@@ -0,0 +1,16 @@
+# Stories URL file
+
+from django.urls import path
+
+from . import views
+
+app_name = 'stories'
+urlpatterns = [
+    path('', views.start, name='start'),
+    path('create_story/', views.create_story, name='create_story'),
+    path('join_story/', views.join_story, name='join_story'),
+    path('roadmap/<int:story_id>/', views.roadmap, name='roadmap'),
+    path('add_segment/<int:story_id>/<int:segment_id>/', views.add_segment, name='add_segment'),
+    path('wait/<int:story_id>/', views.wait, name='wait'),
+    path('finish/<int:story_id>/', views.finish, name='finish'),
+]
